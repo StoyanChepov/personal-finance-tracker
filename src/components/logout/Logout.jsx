8526fc1c22c7cdbfa47524073d2484639fea81ef
StoyanChepov <stoyan.chepov@berncon.com>
2024-11-20 21:55:39 +0200

    Updates

--- src/components/logout/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { LogoutHook } from "../../hooks/useAuthHook";
-
-export default function Logout() {
-  const logout = LogoutHook();
-  logout();
-
-  return <Navigate to="/login" />;
-}
--- src/utils/authUtils.js
+++
@@ -1,9 +0,0 @@
-export const getAccessToken = () => {
-  const authJSON = localStorage.getItem("auth");
-  if (!authJSON) {
-    return null;
-  }
-
-  const authData = JSON.parse(authJSON);
-  return authData?.accessToken;
-};
